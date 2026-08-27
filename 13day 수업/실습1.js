4a49f7bede9a02cce921d63c712109b4dfa6c083
yadaykim <yadaykim@gmail.com>
2026-08-27 09:27:56 +0900

    다올라갈라나?

---
+++ 13day 수업/실습1.js
@@ -0,0 +1,5 @@
+const movieList = document.querySelector("#movie-list");
+const card = document.createElement("div");
+card.textContent = "인셉션";
+card.classList.add("card");
+movieList.append(card);
