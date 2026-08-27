35ea63f989caeafa4c08eaf60eab0dfd30483048
yadaykim-pixel <yadaykim@gmail.com>
2026-08-27 09:16:46 +0900

    Add files via upload

---
+++ 11day 수업/script.js
@@ -0,0 +1,61 @@
+console.log("JavaScript 준비!");
+console.log("JavaScript 요이!");
+console.log("JavaScript 땡!");
+
+const servicename = "my service";
+console.log("my service");
+
+let count = 0;
+console.log(count);
+count = 1;
+console.log(count);
+
+let username;
+username = "yaday";
+console.log(username);
+
+console.log(typeof null);  // object
+
+
+
+
+const 서비스이름 = "ai store";
+console.log(서비스이름);
+const 상품가격 = 39000;
+console.log(상품가격);
+const isLoggedIn = true;
+console.log(isLoggedIn);
+
+let 장바구니개수 = 2;
+console.log(장바구니개수);
+장바구니개수 = 3;
+console.log(장바구니개수);
+
+const numprice = number("100");
+console.log(numprice);
+console.log(typeof numprice);
+
+
+
+
+const title = "인터스텔라"; //영화 제목
+const voteAverage = 8.7; //평균 평점
+const voteCount = 32000; //평가 수
+const popularity = 150.5; //인기도 점수
+const releaseDate = "2014-11-05"; //개봉일
+const originalLanguage = "en"; //원어
+
+console.log(title);
+console.log(voteAverage);
+console.log(voteCount);
+console.log(popularity);
+console.log(releaseDate);
+console.log(originalLanguage);
+
+const additionalVoteCount = 100;
+console.log(voteCount + additionalVoteCount);
+
+const genre = "SF";
+const category = "영화";
+console.log(genre + " " + category);
+
